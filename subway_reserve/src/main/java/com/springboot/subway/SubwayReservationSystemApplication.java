22267e5c9a41b63725ecc1787521b6631563431f
TomZheng <1219.zheng@gmail.com>
2021-04-11 13:54:34 -0700

    read me

---
+++ subway_reserve/src/main/java/com/springboot/subway/SubwayReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.subway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubwayReservationSystemApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        SpringApplication.run(SubwayReservationSystemApplication.class, args);
+    }
+}
